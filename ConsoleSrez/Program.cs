348bfa8a09f7f495e93fee7b1544bbba1f8ec5eb
p1ufi <by.p1ufi@gmail.com>
2022-03-11 13:34:07 +0300

    m

---
+++ ConsoleSrez/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllLibrary;
+using static DllLibrary.Class1;
+
+namespace ConsoleSrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 2020;
+            int group = 1281;
+            string fio = "Лебедева Арина СЕргеевна";
+            var r1 = DllLibrary.Class1.GetStudNumber(year, group, fio);
+            Console.WriteLine(r1);
+            Console.WriteLine("----------------------------");
+            string[] mark = new string[] { "5", "2", "3", "н", "б" };
+            double a = MinAVG(mark);
+            Console.WriteLine(a);
+            Console.WriteLine("----------------------------");
+            Mark m1 = new Mark
+            {
+                Date = DateTime.Now,
+                Estimation = "н"
+            };
+            Mark m2 = new Mark
+            {
+                Date = DateTime.Now,
+                Estimation = "5"
+            };
+            Mark m3 = new Mark
+            {
+                Date = DateTime.Now,
+                Estimation = "н"
+            };
+            Mark m4 = new Mark
+            {
+                Date = DateTime.Now.AddMonths(-1),
+                Estimation = "н"
+            };
+            int[] result = Class1.GetCountTruancy(new List<Mark> { m1, m2, m3, m4 });
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("----------------------------");
+            Mark m5 = new Mark
+            {
+                Date = DateTime.Now,
+                Estimation = "б"
+            };
+            Mark m6 = new Mark
+            {
+                Date = DateTime.Now,
+                Estimation = "5"
+            };
+            Mark m7 = new Mark
+            {
+                Date = DateTime.Now,
+                Estimation = "б"
+            };
+            Mark m8 = new Mark
+            {
+                Date = DateTime.Now.AddMonths(-1),
+                Estimation = "б"
+            };
+            result = Class1.GetCountDisease(new List<Mark> { m5, m6, m7, m8 });
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            Console.ReadKey();
+
+            Student m9 = new Student
+            {
+                FIO = "qwe qwe qwe",
+                GroupNumber = "qwe",
+                Year = 2000
+            };
+            Student m10 = new Student
+            {
+                FIO = "zxc zxc zxc",
+                GroupNumber = "zxc",
+                Year = 2003
+            };
+            var r = Class1.GetMarks(DateTime.Now, new List<Student> { m9, m10 });
+            foreach (var item in r)
+            {
+                Console.WriteLine($"{item.Student.FIO} - {item.Date} - {item.Estimation}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DllLibrary/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DllLibrary
+{
+    public class Class1
+    {
+        public static double MinAVG(string[] marks)
+        {
+            int[] markinmas = marks.Where(x => int.TryParse(x, out int result)).Select(x => int.Parse(x)).ToArray();
+            double avg = Math.Floor(markinmas.Average());
+            return avg;
+        }
+        public class Mark
+        {
+            public DateTime Date { get; set; }
+            public string Estimation { get; set; }
+            public Student Student { get; set; }
+        }
+
+        public class Student
+        {
+            public string FIO { get; set; }
+            public int Year{ get; set; }
+            public string GroupNumber{ get; set; }
+        }
+
+        public static string GetStudNumber(int year, int group, string fio)
+        {
+            string[] initial = fio.Split(' ');
+            return $"{year}.{group}.{initial[0][0]}{initial[1][0]}{initial[2][0]}";
+
+        }
+        public static int[] GetCountTruancy(List<Mark> marks)
+        {
+            
+            int[] estimation = new int[11];
+            for (int i = 0; i < estimation.Length; i++)
+            {
+                estimation[i] = 0;
+            }
+            foreach (var item in marks)
+            {
+                if (item.Estimation == "н")
+                {
+                    estimation[item.Date.Month-1] += 1;
+                }
+            }
+            return estimation;
+        }
+
+       public static int[] GetCountDisease(List<Mark> marks)
+       {
+            int[] estimation = new int[11];
+            for (int i = 0; i < estimation.Length; i++)
+            {
+                estimation[i] = 0;
+            }
+            foreach (var item in marks)
+            {
+                if (item.Estimation == "б")
+                {
+                    estimation[item.Date.Month - 1] += 1;
+                }
+            }
+            return estimation;
+       }
+
+        public static List<Mark> GetMarks(DateTime now, List<Student> students)
+        {
+            
+            List<Mark> result = new List<Mark>();
+            foreach (var student in students)
+            {
+                DateTime currentDate = now.Date;
+                for (int i = 0; i < 11; i++)
+                {
+                    result.Add(new Mark
+                    {
+                        Date = currentDate,
+                        Estimation = new Random().Next(2, 5).ToString(),
+                        
+                        Student = student
+                    });
+                    currentDate = currentDate.AddDays(1);
+                }
+            }
+            return result;
+        }
+    }
+}
